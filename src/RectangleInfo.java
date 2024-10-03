9813a6b30e11ae168b31765fcc2f89486c0eab8b
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-03 14:12:13 -0400

    New code for task 3, not done yet

---
+++ src/RectangleInfo.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class RectangleInfo {
+
+
+
+
+
+
+
+
+
+
+        public static void main(String[] args)
+        {
+            Scanner in = new Scanner(System.in);
+            double lengthNumber = 0.00;
+            double widthNumber = 0.00;
+            double areaNumber = 0.00;
+            double perimeterNumber = 0.00;
+            String trash = "";
+
+            boolean done = false;
+            do
+            {
+                System.out.print("Please enter the length of the rectangle: ");
+                if(in.hasNextDouble())
+                {
+                    lengthNumber = in.nextDouble();
+                    in.nextLine();
+                    done = true;
+                }
+                else
+                {
+
+                    trash = in.nextLine();
+                    System.out.println("You said your length: " + trash);
+                    System.out.println("Please Input a valid amount");
+                    done = false;
+                }
+            }while(!done);
+            do
+            {
+                System.out.print("Please enter the width of the rectangle: ");
+                if(in.hasNextDouble())
+                {
+                    widthNumber = in.nextDouble();
+
+                    in.nextLine();
+                    done = true;
+                }
+                else
+                {
+
+                    trash = in.nextLine();
+                    System.out.println("You said your width was: " + trash);
+                    System.out.println("Please Input a valid amount");
+                    done = false;
+                }
+            }while(!done);
+            areaNumber = lengthNumber * widthNumber;
+            perimeterNumber = lengthNumber * 2 + widthNumber * 2;
+            System.out.println("The area of your rectangle is: " + areaNumber);
+            System.out.println("The perimeter of your rectangle is: " + perimeterNumber);
+
+        }
+
+
+
+
+
+
+
+}
