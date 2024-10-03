8c0f987af22d7e1a1b3406f181ce6587bbf869cb
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-03 15:11:57 -0400

    task 4 code written, not tested yet

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -2,15 +2,6 @@ import java.util.Scanner;
 
 public class RectangleInfo {
 
-
-
-
-
-
-
-
-
-
         public static void main(String[] args)
         {
             Scanner in = new Scanner(System.in);
